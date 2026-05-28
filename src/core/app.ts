d98498c4296fdc63d11de27cbd0ed3700cdff1fc
Dacuvis <rayfem24@gmail.com>
2026-05-28 19:08:21 +0700

    Update Response

--- src/core/app.ts
+++ src/core/app.ts
@@ -1,70 +1,104 @@
-    import { Context } from "./context";
-    import { Router } from "./router";
+import { Context } from "./context";
+import { Router } from "./router";
 
-    export class Haxxor {
-        router: Router
+export class Haxxor {
+  router: Router;
 
-        constructor() {
-            this.router = new Router()
+  constructor() {
+    this.router = new Router();
+  }
+
+  // Support for plugins
+  use(plugin: Haxxor) {
+    this.router.routes.push(...plugin.router.routes);
+    return this;
+  }
+
+  // HTTP method(GET) shortcuts
+  get(path: string, handler: Function) {
+    this.router.add("GET", path, handler);
+    return this;
+  }
+
+  // HTTP method(POST) shortcuts
+  post(path: string, handler: Function) {
+    this.router.add("POST", path, handler);
+    return this;
+  }
+
+  // HTTP method(PUT) shortcuts
+  put(path: string, handler: Function) {
+    this.router.add("PUT", path, handler);
+    return this;
+  }
+
+  // HTTP method(DELETE) shortcuts
+  delete(path: string, handler: Function) {
+    this.router.add("DELETE", path, handler);
+    return this;
+  }
+
+  // Start the server
+  listen(port: number) {
+    Bun.serve({
+      port,
+      fetch: async (req) => {
+        const url = new URL(req.url);
+        const route = this.router.find(req.method, url.pathname);
+
+        if (!route) {
+          return new Response("Not Found", { status: 404 });
         }
 
-        // Support for plugins
-        use(plugin: Haxxor) {
-            this.router.routes.push(...plugin.router.routes)
-            return this
+        const query: Record<string, string> = {};
+        url.searchParams.forEach((value, key) => {
+          query[key] = value;
+        });
+
+        let bodyData: any = null;
+        if (["POST", "PUT"].includes(req.method)) {
+          const contentType = req.headers.get("content-type") || "";
+          try {
+            if (contentType.includes("application/json")) {
+              bodyData = await req.json();
+            } else if (
+              contentType.includes("application/x-www-form-urlencoded")
+            ) {
+              const formData = await req.formData();
+              bodyData = {};
+
+              formData.forEach((value, key) => {
+                bodyData[key] = value;
+              })
+            }
+          } catch (error) {
+            return new Response("Bad Request", { status: 400 });
+          }
         }
+        const ctx: any = new Context(req, route?.params, query);
 
-        // HTTP method(GET) shortcuts
-        get(path: string, handler: Function) {
-            this.router.add('GET', path,handler)
-            return this
+        (ctx as any).request = {
+          body: bodyData,
+        };
+
+        try {
+          const result = await route.handler(ctx);
+
+          if (result instanceof Response) {
+            return result;
+          }
+          if (typeof result === "string") {
+            return ctx.text(result);
+          }
+          if (typeof result === "object") {
+            return ctx.json(result);
+          }
+        } catch (error) {
+          return ctx.status(500).text("Internal Server Error");
         }
+      },
+    });
 
-        // HTTP method(POST) shortcuts
-        post(path: string, handler: Function) {
-            this.router.add('POST', path, handler)
-            return this
-        }
-
-        // HTTP method(PUT) shortcuts
-        put(path: string, handler: Function) {
-            this.router.add('PUT', path, handler)
-            return this
-        }
-
-        // HTTP method(DELETE) shortcuts
-        delete(path: string, handler: Function) {
-            this.router.add('DELETE', path, handler)
-            return this
-        }
-
-        // Start the server
-        listen(port: number) {
-            Bun.serve({
-                port,
-                fetch: async (req) => {
-                    const url = new URL(req.url)
-                    const route = this.router.find(req.method, url.pathname)
-
-                    if (!route) {
-                    return new Response('Not Found', { status: 404 })
-                    }
-
-                    const query: Record<string, string> = {}
-                    url.searchParams.forEach((value, key) => {
-                        query[key] = value
-                    })
-                    
-                    const ctx = new Context(req, route?.params, query)
-                    
-                    try {
-                        return await route.handler(ctx)
-                    } catch (error) {
-                        return ctx.status(500).text('Internal Server Error')
-                    }
-                }
-            })
-
-            console.log(`Server is running on port ${port}`)
-        }
-    }
+    console.log(`Server is running on port ${port}`);
+  }
+}
--- test/example.routes.ts
+++ test/example.routes.ts
@@ -1,21 +1,8 @@
 import { Haxxor } from "../src/core/app";
 
-export const exampleRoutes = new Haxxor().get("/products", (ctx: any) => {
-  const search = ctx.query.search ?? "semua barang";
-  const page = ctx.query.page ?? "1";
-  const limit = ctx.query.limit ?? "10";
-
-  return ctx.json({
-    success: true,
-    message: "Berhasil mengambil data produk",
-    filter: {
-      keyword: search,
-      halaman: parseInt(page),
-      per_halaman: parseInt(limit),
-    },
-    data: [
-      { id: 1, nama: "Sepatu Keren", harga: 250000 },
-      { id: 2, nama: "Baju Santai", harga: 120000 },
-    ],
-  });
-});
+export const exampleRoutes = new Haxxor()
+    .get("/hello", () => {
+        return {
+            message: "Hello, World!"
+        }
+    })
